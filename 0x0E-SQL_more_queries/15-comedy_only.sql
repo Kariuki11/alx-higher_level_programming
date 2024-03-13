b85127b37e0253aae36f21163d35095426b33fe0
kariuki11 <irungukenneth433@gmail.com>
2024-03-13 09:53:15 -0700

    The cody show file

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- list all the Comedy shows in the database hbtn_0d_tvshows
+-- lists all the rows of a database corresponding to a column value
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
