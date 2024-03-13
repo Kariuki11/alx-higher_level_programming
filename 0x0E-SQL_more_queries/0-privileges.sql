f8ba365f1613a79cb8550e6fbf51adad25bf6a4c
kariuki11 <irungukenneth433@gmail.com>
2024-03-13 07:54:42 -0700

    The Querries project file 1

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script that lists all the priveleges of MySql users
+-- for user_0d_1 and user_0d_2 on my server
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
