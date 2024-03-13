71727e0e390a48267a4f4d7097d421548bb174cf
kariuki11 <irungukenneth433@gmail.com>
2024-03-13 08:17:47 -0700

    The Priviledges file

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script that list all the priveleges of MySql users
+-- script that list all priveleges of MySql users
 -- for user_0d_1 and user_0d_2 on mySql server
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
