b8b9cc392ad6dcf1b598b9d94f80cb24d3d252ca
kariuki11 <irungukenneth433@gmail.com>
2024-03-13 07:27:06 -0700

    The Avg temp file

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- computes the score average of all records in the table second_table of the database hbtn_0c_0 in MySQL server.
+SELECT city, AVG( value ) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
